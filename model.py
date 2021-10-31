c5b01e123099cf51b072b1cec2f505a308c04d2f
charliethomasct82 <charliethomas.ct82@gmail.com>
2021-10-31 23:20:23 +0530

    few more code added

---
+++ model.py
@@ -0,0 +1,2 @@
+print('hellow,how are you')
+print("adding more code to make the program filled with code. there is nothing logical here just trying to learn pull and compare functionis the git hub")
